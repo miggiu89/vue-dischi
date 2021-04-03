b99e3d318feace2ada9b7175e36416fba0b4b788
Matteo <matteomigliori280489@gmail.com>
2021-04-03 10:53:09 +0200

    es base ok, bonus 2 ok, bonus 1 no.

--- vue/script.js
+++ vue/script.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: "#root",
     data:{
         albums: '',
+        valType:'',
+        musicType: [],
     },
 
     mounted(){
@@ -9,23 +11,24 @@ var app = new Vue({
             .get('https://flynn.boolean.careers/exercises/api/array/music')
             .then((result) => {
             this.albums = result.data.response;
+
+            this.filter();
+            this.order();
             })
     },
 
+    methods: {
+        filter(){
+            this.albums.forEach(element =>{
+                if(this.musicType.includes(element.genre)){
+                    this.musicType.push(element.genre);
+                }
+            });
+        },
+        order(){
+            this.albums.sort((albumA,albumB) => albumA.year - albumB.year)
+        }
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+});
